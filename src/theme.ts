a9fc4f432a1dd5baa1d222379bd3aea10feead47
stanislavtrubitsyn <stanislavtrubitsyn@gmail.com>
2025-06-02 12:58:25 +0300

    add breakpoints

--- src/theme.ts
+++ src/theme.ts
@@ -1,11 +1,24 @@
 import { createTheme } from '@mui/material/styles'
 
+const breakpoints = {
+	values: {
+		xxs: 0,
+		xs: 481,
+		sm: 769,
+		md: 1025,
+		lg: 1281,
+		xl: 1441,
+		xxl: 1921,
+	},
+}
+
 export const lightTheme = createTheme({
 	palette: {
 		mode: 'light',
 		primary: { main: '#1976d2' },
 		background: { default: '#f4f4f4', paper: '#fff' },
 	},
+	breakpoints,
 })
 
 export const darkTheme = createTheme({
@@ -14,4 +27,5 @@ export const darkTheme = createTheme({
 		primary: { main: '#90caf9' },
 		background: { default: '#121212', paper: '#1e1e1e' },
 	},
+	breakpoints,
 })
