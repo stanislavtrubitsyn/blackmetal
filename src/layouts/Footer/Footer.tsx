8740844699a02a55f18bd4e6b5caa582815f4ec0
stanislavtrubitsyn <stanislavtrubitsyn@gmail.com>
2025-06-04 11:52:45 +0300

    upd sizes footer

--- src/layouts/Footer/Footer.tsx
+++ src/layouts/Footer/Footer.tsx
@@ -1,12 +1,4 @@
-import {
-	Box,
-	List,
-	ListItem,
-	Typography,
-	Link as MUILink,
-	Divider,
-	Button,
-} from '@mui/material'
+import { Box, List, ListItem, Typography, Link as MUILink, Divider, Button } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { SocialLinks, UniversalLogo } from '@/components'
 import routes from '@/router/routes.json'
@@ -54,96 +46,113 @@ const Footer = () => {
 	return (
 		<Box
 			sx={{
-				width: '1440px',
+				width: '100%',
+				display: 'flex',
+				justifyContent: 'center',
 				minHeight: '271px',
 				px: '190px',
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
+
 				bgcolor: '#ffffff',
 			}}
 		>
-			<List sx={{ display: 'flex', gap: '60px', p: 0, pt: '20px' }}>
-				{footerLinks.map(link => (
-					<ListItem
-						key={link.path}
-						component={Link}
-						to={link.path}
-						sx={listItemStyle}
-					>
-						<Typography sx={listTypographyStyle}>{link.title}</Typography>
-					</ListItem>
-				))}
-			</List>
-			<Divider
-				sx={{
-					width: '100%',
-					borderColor: '#D2D2D2',
-					borderBottomWidth: '1px',
-					mt: 2,
-				}}
-			/>
 			<Box
 				sx={{
-					mt: '25px',
+					maxWidth: 'xxl',
+					minWidth: 'xxs',
+					width: '100%',
 					display: 'flex',
-					flexDirection: 'row',
-					justifyContent: 'center',
-					gap: '70px',
+					flexDirection: 'column',
+					alignItems: 'center',
 				}}
 			>
-				<Box>
-					<UniversalLogo size={54} />
-					<MUILink href='' target='_blank' rel='noopener noreferrer'>
-						<Typography sx={{ fontSize: '12px' }}>Мапа сайту</Typography>
-					</MUILink>
-					<Typography sx={{ fontSize: '12px' }}>© 2006-2019 О сайте</Typography>
-				</Box>
+				<List
+					sx={{
+						display: 'flex',
+						flexDirection: 'row',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+						width: '80%',
+						// gap: '60px',
+						p: 0,
+						pt: '20px',
+					}}
+				>
+					{footerLinks.map(link => (
+						<ListItem key={link.path} component={Link} to={link.path} sx={listItemStyle}>
+							<Typography sx={listTypographyStyle}>{link.title}</Typography>
+						</ListItem>
+					))}
+				</List>
+				<Divider
+					sx={{
+						width: '100%',
+						maxWidth: 'xxxl',
+						minWidth: 'xxs',
+						borderColor: '#D2D2D2',
+						borderBottomWidth: '1px',
+						mt: 2,
+					}}
+				/>
+				<Box
+					sx={{
+						mt: '25px',
+						display: 'flex',
+						width: '95%',
+						flexDirection: 'row',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						// gap: '70px',
+					}}
+				>
+					<Box>
+						<UniversalLogo size={54} />
+						<MUILink href='' target='_blank' rel='noopener noreferrer'>
+							<Typography sx={{ fontSize: '12px' }}>Мапа сайту</Typography>
+						</MUILink>
+						<Typography sx={{ fontSize: '12px' }}>© 2006-2019 О сайте</Typography>
+					</Box>
 
-				<Box>
-					<Box sx={boxStyles}>
-						<Typography sx={titleStyles}>МІСЦЕ ЗНАХОДЖЕННЯ</Typography>
+					<Box>
+						<Box sx={boxStyles}>
+							<Typography sx={titleStyles}>МІСЦЕ ЗНАХОДЖЕННЯ</Typography>
+						</Box>
+						<Typography sx={{ fontSize: '15px', fontWeight: 400, color: '#606060' }}>
+							Дніпро, проспект Дмитра <br /> Яворницького, 19
+						</Typography>
 					</Box>
-					<Typography
-						sx={{ fontSize: '15px', fontWeight: 400, color: '#606060' }}
-					>
-						Дніпро, проспект Дмитра <br /> Яворницького, 19
-					</Typography>
-				</Box>
-				<Box>
-					<Box sx={boxStyles}>
-						<Typography sx={titleStyles}>КОНТАКТИ</Typography>
+					<Box>
+						<Box sx={boxStyles}>
+							<Typography sx={titleStyles}>КОНТАКТИ</Typography>
+						</Box>
+						<Typography sx={{ fontSize: '15px', fontWeight: 400, color: '#333333' }}>
+							+38 096 786 77 77 <br /> ntu@gmail.com
+						</Typography>
 					</Box>
-					<Typography
-						sx={{ fontSize: '15px', fontWeight: 400, color: '#333333' }}
-					>
-						+38 096 786 77 77 <br /> ntu@gmail.com
-					</Typography>
-				</Box>
-				<Box>
-					<Box sx={boxStyles}>
-						<Typography sx={titleStyles}>ЗАДАТИ ПИТАННЯ</Typography>
+					<Box>
+						<Box sx={boxStyles}>
+							<Typography sx={titleStyles}>ЗАДАТИ ПИТАННЯ</Typography>
+						</Box>
+						<Button
+							variant='outlined'
+							sx={{
+								width: '186px',
+								borderRadius: 0,
+								textTransform: 'none',
+								borderColor: '#333333',
+								fontSize: '14px',
+								fontWeight: 400,
+								color: '#333333',
+							}}
+						>
+							Зворотній зв'язок
+						</Button>
 					</Box>
-					<Button
-						variant='outlined'
-						sx={{
-							width: '186px',
-							borderRadius: 0,
-							textTransform: 'none',
-							borderColor: '#333333',
-							fontSize: '14px',
-							fontWeight: 400,
-							color: '#333333',
-						}}
-					>
-						Зворотній зв'язок
-					</Button>
-				</Box>
-				<Box>
-					<Box sx={boxStyles}>
-						<Typography sx={titleStyles}>СОЦ.МЕРЕЖІ</Typography>
+					<Box>
+						<Box sx={boxStyles}>
+							<Typography sx={titleStyles}>СОЦ.МЕРЕЖІ</Typography>
+						</Box>
+						<SocialLinks />
 					</Box>
-					<SocialLinks />
 				</Box>
 			</Box>
 		</Box>
