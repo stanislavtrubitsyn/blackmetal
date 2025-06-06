0795accc28daab56586d73d2ad651fef3cd16ff4
stanislavtrubitsyn <stanislavtrubitsyn@gmail.com>
2025-06-06 12:41:54 +0300

    Updates

--- assets/index-BxDslJkJ.js
+++
@@ -1,1 +0,0 @@
-import{j as u,T as r}from"./index-Bp1RSzNC.js";const a=()=>u.jsx(r,{variant:"h4",children:"Ошибка 404 - Страница не найдена"});export{a as default};
--- assets/index-DQXW_q-C.js
+++
@@ -1,1 +0,0 @@
-import{j as u,T as a}from"./index-Bp1RSzNC.js";const s=()=>u.jsx(a,{variant:"h4",children:"Панель управления (только для авторизованных)"});export{s as default};
