ff6d680e4ab08b290300881bdfc8dfebcefebb8a
stanislavtrubitsyn <stanislavtrubitsyn@gmail.com>
2025-06-21 12:59:43 +0300

    .

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 import { Header } from '@/layouts'
 import { Footer } from '@/layouts'
-import { Box } from '@mui/material'
+import { Box, useTheme, useMediaQuery } from '@mui/material'
 import LatestNews from '@/components/LatestNews'
 import { useLocation } from 'react-router-dom'
 import routes from '@/router/routes.json'
@@ -12,13 +12,24 @@ interface MainLayoutProps {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
 	const { pathname } = useLocation()
+	const theme = useTheme()
+	const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
 
 	const showLatest = pathname !== routes.HomePage.path && pathname !== routes.ContactsPage.path
 
 	return (
 		<Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
 			<Header />
-			<Box component='main' sx={{ display: 'flex', flex: 1, width: '100%', m: '0 auto' }}>
+			<Box
+				component='main'
+				sx={{
+					display: 'flex',
+					flex: 1,
+					width: '100%',
+					m: '0 auto',
+					flexDirection: isMobile ? 'column-reverse' : 'row',
+				}}
+			>
 				{showLatest && <LatestNews />}
 				<Box sx={{ flex: 1 }}>{children}</Box>
 			</Box>
